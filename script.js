ad696d5b8cdf01860241d5324fc8ff3bcf0e8f7f
Amir Fakhrullah <amirfakhrullah96@gmail.com>
2020-12-12 10:04:16 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+const generateTarget = () => Math.floor(Math.random() * 9 + 1);
+
+const compareGuesses = (humanGuess, computerGuess, secretNumber) => {
+  if (Math.abs(humanGuess - secretNumber) < Math.abs(computerGuess - secretNumber)) {
+    return true;
+  } else {
+    return false;
+  };
+}
+
+const updateScore = winner => {
+  if (winner === 'human') {
+    humanScore += 1;
+  } else if (winner === 'computer') {
+    computerScore += 1;
+  };
+}
+
+const advanceRound = () => {
+  currentRoundNumber += 1;
+}
